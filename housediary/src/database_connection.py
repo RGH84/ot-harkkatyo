746d6f61d2652f527197eb0ea078d8fa8e37f8bb
RGH84 <jhmkemppainen16@gmail.com>
2024-05-06 11:14:57 +0300

    Add .env

--- housediary/src/database_connection.py
+++ housediary/src/database_connection.py
@@ -1,16 +1,11 @@
-import os
 import sqlite3
+from config import DATABASE_FILE_PATH, TEST_DATABASE_FILE_PATH
 
-dirname = os.path.dirname(__file__)
-
-connection = sqlite3.connect(os.path.join(
-    dirname, "..", "data", "database.sqlite"))
+connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
 
-test_connection = sqlite3.connect(os.path.join(
-    dirname, "..", "data", "test_database.sqlite"))
+test_connection = sqlite3.connect(TEST_DATABASE_FILE_PATH)
 test_connection.row_factory = sqlite3.Row
-
 
 def get_database_connection():
     return connection
